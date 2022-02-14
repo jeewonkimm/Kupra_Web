1a95334d2bc45bd7b096df994e2fd7da673eee7a
Jeewon <wldnjsqnsl@gmail.com>
2022-02-14 19:18:01 +0900

    User 모델 구현
    Signed-off-by: Jeewon <wldnjsqnsl@gmail.com>

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -6,7 +6,7 @@ class UserStorage{
     static #users = {
         id: ["woorimIT", "나개발", "김팀장"],
         psword: ["1234", "1234", "123456"],
-        name: ["우리밋","나개발","김팀장"],
+        nams: ["우리밋","나개발","김팀장"],
     };
 
     static getUsers(...fields){
@@ -19,6 +19,18 @@ class UserStorage{
         },{});
     
         return newUsers;
+    }
+
+    static getUserInfo(id){
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users);
+        const userInfo = usersKeys.reduce((newUser,info)=>{
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+
+        return userInfo;
     }
 }
 
